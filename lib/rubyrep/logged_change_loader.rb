9673eaccd1acfef61b5e143b937bce9771cd7bbe
Brandon Dunne <bdunne@redhat.com>
2014-08-01 17:43:23 -0400

    PAUSED: Use `git resume` to continue working.

--- lib/rubyrep/logged_change_loader.rb
+++ lib/rubyrep/logged_change_loader.rb
@@ -188,6 +188,7 @@ $log.info("XXXXX XXXXX #{self.class.name}##{__method__} CHANGE ARRAY LENGTH #{ch
           $log.warn("XXXXX #{self.class.name}##{__method__} OPEN TRANSACTIONS #{connection.connection.open_transactions.inspect}")
           begin
           $log.warn("XXXXX #{self.class.name}##{__method__} OPEN TRANSACTIONS #{connection.connection.open_transactions.inspect}")
+          $log.warn("XXXXX #{self.class.name}##{__method__} OUTSIDE TRANSACTION? #{connection.connection.outside_transaction?.inspect}")
             $log.warn("XXXXX #{self.class.name}##{__method__} DELETING RECORD #{change['id'].inspect} from #{change_log_table.inspect}")
             # $log.warn("XXXXX #{self.class.name}##{__method__} Connection #{connection.inspect}")
             connection.execute "delete from #{change_log_table} where id = #{change['id']};"
