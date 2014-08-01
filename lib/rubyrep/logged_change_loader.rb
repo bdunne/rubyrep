f0521b34817613272ab112f2ab3c1fd1749454b4
Brandon Dunne <bdunne@redhat.com>
2014-08-01 16:59:51 -0400

    PAUSED: Use `git resume` to continue working.

--- lib/rubyrep/logged_change_loader.rb
+++ lib/rubyrep/logged_change_loader.rb
@@ -189,7 +189,7 @@ $log.info("XXXXX XXXXX #{self.class.name}##{__method__} CHANGE ARRAY LENGTH #{ch
           begin
           $log.warn("XXXXX #{self.class.name}##{__method__} OPEN TRANSACTIONS #{connection.connection.open_transactions.inspect}")
             $log.warn("XXXXX #{self.class.name}##{__method__} DELETING RECORD #{change['id'].inspect} from #{change_log_table.inspect}")
-            connection.execute "delete from #{change_log_table} where id = #{change['id']}"
+            connection.execute "delete from #{change_log_table} where id = #{change['id']};"
             $log.warn("XXXXX #{self.class.name}##{__method__} DELETED RECORD")
           rescue Exception => err
             $log.warn("XXXXX #{self.class.name}##{__method__} FAILED TO DELETE RECORD #{err.inspect}")
